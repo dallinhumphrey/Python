f59c99c3cdfde49cfc4c823bb08f84676c5f504f
dallinhumphrey <dallinhumphrey@gmail.com>
2020-02-25 11:54:01 -0700

    Make a number negative

--- python.py
+++ python.py
@@ -1,19 +1,11 @@
-starter_sentence = 'The quick brown fox jumped'
-new_sentence = 'Thy' + starter_sentence[3:]
-print(new_sentence)
+"""
+write me a program that makes an integer negative
+"""
+
+x = -5
+absolute_value = abs(-x)
+
+negative_x = absolute_value * -1
 
 
-sentence = 'The quick brown fox jumped'
-sentence_two = sentence.upper()
-
-print(sentence)
-print(sentence_two)
-
-sentence = 'the quick brown fox jumped'.capitalize()
-print(sentence)
-
-sentence = 'the quick brown fox jumped'.title()
-print(sentence)
-
-sentence = 'the Quick Brown fOx jumped'
-print(sentence.lower())
+print(negative_x)
