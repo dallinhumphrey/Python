fc03d76489c444ecd1e539b70961f3548ee6158e
dallinhumphrey <dallinhumphrey@gmail.com>
2020-02-25 11:25:53 -0700

    practice 2

--- python.py
+++ python.py
@@ -1,3 +1,19 @@
 starter_sentence = 'The quick brown fox jumped'
 new_sentence = 'Thy' + starter_sentence[3:]
 print(new_sentence)
+
+
+sentence = 'The quick brown fox jumped'
+sentence_two = sentence.upper()
+
+print(sentence)
+print(sentence_two)
+
+sentence = 'the quick brown fox jumped'.capitalize()
+print(sentence)
+
+sentence = 'the quick brown fox jumped'.title()
+print(sentence)
+
+sentence = 'the Quick Brown fOx jumped'
+print(sentence.lower())
