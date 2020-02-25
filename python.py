dae7f09ac76f8a117b44ff9f4ef0cc1848e45eb5
dallinhumphrey <dallinhumphrey@gmail.com>
2020-02-25 15:29:03 -0700

    dynamic reducer

--- python.py
+++ python.py
@@ -1,11 +1,21 @@
-"""
-write me a program that makes an integer negative
-"""
-
-x = -5
-absolute_value = abs(-x)
-
-negative_x = absolute_value * -1
+import operator
+from functools import reduce
 
 
-print(negative_x)
+def dynamic_reducer(collection, op):
+    operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv,
+    }
+
+    return reduce((lambda total, element: operators[op](total, element)), collection)
+
+
+total = dynamic_reducer([1, 2, 3], '/')  # 0.16666666666666666
+
+print(dynamic_reducer([1, 2, 3], '+'))
+print(dynamic_reducer([1, 2, 3], '-'))
+print(dynamic_reducer([1, 2, 3], '*'))
+print(dynamic_reducer([1, 2, 3], '/'))
